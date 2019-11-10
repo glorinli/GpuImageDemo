17aea4926921e929d527c46358ae95f8c70c10d4
glorinli <ligr1991@gmail.com>
2019-11-10 22:30:09 +0800

    Add button.

--- app/src/main/java/xyz/dogold/gpuimagedemo/MainActivity.java
+++ app/src/main/java/xyz/dogold/gpuimagedemo/MainActivity.java
@@ -1,8 +1,12 @@
 package xyz.dogold.gpuimagedemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import xyz.dogold.gpuimagedemo.ui.VideoFilterActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnImageFilter:
+                break;
+            case R.id.btnVideoFilter:
+                startActivity(new Intent(this, VideoFilterActivity.class));
+                break;
+        }
     }
 }
